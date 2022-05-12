a9e1b974720d3d856b3b7d33b24e7640279fd9f2
phenomenalMatrix <mrflaitx@gmail.com>
2022-05-12 19:58:57 +0600

    add space

--- app/src/main/java/com/mrflaitx/animatebtn/MainActivity.java
+++ app/src/main/java/com/mrflaitx/animatebtn/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setStartTransparent();
         initListeners();
+
+
     }
 
     private void initListeners() {
